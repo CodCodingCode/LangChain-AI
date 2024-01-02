b6cea11c1d7df45eea8120d7d99a7d5b91957245
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2024-01-02 09:11:52 -0500

    Add files via upload

---
+++ Replicate#2/htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 25%;
+}
+.chat-message .avatar img {
+  max-width: 70px;
+  max-height: 70px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://img.freepik.com/free-vector/vintage-robot-toy-white-background_1308-77501.jpg?size=626&ext=jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.242596085.1700890780&semt=sph">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
